f8727fd579e6cf01ff1a1d74fc308020cef2e824
ShoMasegi <m2.b0c27@gmail.com>
2018-10-19 18:24:12 +0900

    create project

---
+++ ShoMasegi/Gist/Gist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gist
+//
+//  Created by 柵木奨 on 2018/10/19.
+//  Copyright © 2018年 ShoMasegi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
